d89e8c492495aca8cf519ea9a8ec0cab65f5c384
Gregory-Vinokur <grisha-vinokur@mail.ru>
2023-02-28 17:25:20 +0300

    fix: change news API to newscatcher API for Netlify deploy

--- src/data/news_api/sport_api.ts
+++ src/data/news_api/sport_api.ts
@@ -1,38 +1,43 @@
 import { database } from '../../server/firebaseAuth';
 import sport_logo from '../../../assets/img/sport_logo.jpg';
 
-async function loadSportNews() {
-    const apiKey = '7c9fb6fc26e645ae8262723d2624e6d8';
-    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=gb&category=sports&apiKey=${apiKey}`);
+async function loadSportNewsSources() {
+    const apiKey = 'BWEFPCAvgxeSrSfqEIYkU855j-QX3LzB8pg9AEUpePQ';
+    const response = await fetch(`https://api.newscatcherapi.com/v2/latest_headlines?countries=GB&topic=sport`, {
+        headers: {
+            'x-api-key': apiKey
+        }
+    });
     const data = await response.json();
     return data.articles;
 }
 
 export const generateSportPost = async () => {
     try {
-        const articles = await loadSportNews();
-        if (!articles || articles.length === 0) {
-            throw new Error('No sports news found');
-        } else if (articles || articles.length > 0) {
-            const article = articles[Math.floor(Math.random() * articles.length)];
-            const data = {
-                author: "Sport News",
-                date: Date.now(),
-                text: article.title,
-                image: article.urlToImage,
-                likes: 0,
-                shares: 0,
-                logo: sport_logo,
-                comments: [],
-                liked: '',
-                reposted: "",
-                id: ""
-            };
-            const response = await database.ref("posts/").push(data);
-            console.log("Post saved successfully: ", response);
+        const sources = await loadSportNewsSources();
+        if (!sources || sources.length === 0) {
+            throw new Error('No sports news sources found');
         }
+        const source = sources[Math.floor(Math.random() * sources.length)];
+        const post = {
+            author: "Sport News",
+            date: Date.now(),
+            text: source.title,
+            image: source.media || source.image,
+            likes: 0,
+            shares: 0,
+            logo: sport_logo,
+            comments: [],
+            liked: '',
+            reposted: "",
+            id: ""
+        };
+        const postRef = database.ref("posts/").push();
+        await postRef.set(post);
+        console.log("Post saved successfully: ", postRef.key);
     } catch (error) {
         console.error("Error generating sport post: ", error);
     }
 };
+
 
